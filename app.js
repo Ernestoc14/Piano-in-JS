8113ccc179ea0246e7f0f7dd944dd41390f69a55
Ernestoc14 <descreed34@hotmail.com>
2022-09-25 21:34:14 -0500

    Add Keys Sounds and JS

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const x = 3;
-let va = 2.0;
+const pianoKeys = document.querySelectorAll('.key')
+
+function playSound(newUrl){ //Function to play the sound
+    console.log(newUrl)
+    new Audio(newUrl).play() //Get the new audio after looping in pianoKeys
+}
+
+pianoKeys.forEach((pianoKey, i) =>{   //Looping to create a new URL
+    const number = i < 9 ? '0'+ (i + 1)  : (i + 1) // if i is smaller than 9 then (?) return 0 plus i+1 ELSE return i+1
+    const newUrl = '24-piano-keys/key' + number + '.mp3'
+    pianoKey.addEventListener('click', () => playSound(newUrl)) //Callback
+})
